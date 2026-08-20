c8f955690b580d0c41aa61db4c64e17f5d6fedf3
Alim Nadap <alimndpjob@gmail.com>
2026-08-20 14:47:40 +0530

    `share_plus` was upgraded from 7.2.2 to __10.1.4__ which supports the newer Android SDK. All dependencies resolved successfully.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,7 +14,19 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // Force all subprojects (including plugins like share_plus) to compile against SDK 36
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val androidExt = project.extensions.findByName("android")
+            if (androidExt != null) {
+                val android = androidExt as com.android.build.gradle.BaseExtension
+                android.compileSdkVersion(36)
+            }
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
